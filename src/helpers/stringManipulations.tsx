f77493998015b883fd1366d33b52b7627d8f7c49
DigitalSloth <digitals1oth@proton.me>
2024-06-16 15:59:51 +0100

    feature: new helper functions

--- src/helpers/stringManipulations.tsx
+++ src/helpers/stringManipulations.tsx
@@ -10,3 +10,20 @@ export function generateRandomString(length: number) {
 	}
 	return result;
 }
+
+export function getReadTimeAttribute(content: string) {
+	const wordsPerMin = 300;
+	content = content.replace(/<[^>]*>/g, "");
+	const numWords = content.split(" ").length;
+	const readTime = numWords / wordsPerMin;
+
+	return Math.ceil(readTime);
+}
+
+export function generateExcerpt(content: string, length: number): string {
+	const contentWithoutTags = content.replace(/<[^>]+>/g, '');
+	const excerpt = contentWithoutTags.substr(0, length);
+	const finalExcerpt = excerpt.substr(0, Math.min(excerpt.length, excerpt.lastIndexOf(" "))) + '...';
+
+	return finalExcerpt;
+}
